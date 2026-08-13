7cab1b3c5465ac3e984c03e5f3b7dbf23760250b
thiagopolezasouza-create <thiago.poleza.souza@escola.pr.gov.br>
2026-08-13 10:12:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// Seleciona todos os botões de curtir e amei
+const botoes = document.querySelectorAll("button");
 
+botoes.forEach(function (botao) {
+    let curtiu = false;
+    
+    botao.addEventListener("click", function() {
+        console.log("Interação com o post detectada");
+        
+        let contador = botao.querySelector("span");
+        
+        if (curtiu === false) {
+            contador.textContent++;
+            curtiu = true;
+            botao.style.backgroundColor = "#ff0000"; // Muda para vermelho ao clicar
+        } else {
+            contador.textContent--;
+            curtiu = false;
+            botao.style.backgroundColor = "#8932a1"; // Volta para a cor original
+        }
+    });
+});
